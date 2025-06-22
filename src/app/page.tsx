6edee83c68bd290422db5ed0d0b4e693f9125f6a
as dsf <adsf38759@gmail.com>
2025-06-22 06:24:45 +0000

    ahora hagamos doce pads con las notas musicales C, C#, D, D# E... ya que

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { NotePads } from '@/components/note-pads';
 import { Tuner } from '@/components/tuner';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
         <p className="text-muted-foreground mt-2">Sing or play a note, and we'll tell you if you're in tune.</p>
       </div>
       <Tuner />
+      <NotePads />
     </main>
   );
 }
