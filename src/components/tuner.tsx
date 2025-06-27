f239edcca64ef762211451d2d59168fd629ba757
as dsf <adsf38759@gmail.com>
2025-06-27 23:00:58 +0000

    25 cents hacia arriba y abajo para todos y para las notas graves 30 cent

--- src/components/tuner.tsx
+++ src/components/tuner.tsx
@@ -292,7 +292,7 @@ export function Tuner({ notePool, gender }: { notePool: NoteInfo[]; gender: 'mas
     return () => window.removeEventListener('resize', handleResize);
   }, [challengeNotes.length]);
 
-  const tolerance = 25;
+  const tolerance = activeNote && activeNote.midi <= 48 ? 30 : 25;
 
   useEffect(() => {
     if (!isDetecting || !activeNote || lastCompletedNoteFullName || sessionCompleted) {
