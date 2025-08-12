0ca2f695060c8814459d4c0ce7600a679eaf4e6f
as dsf <adsf38759@gmail.com>
2025-08-12 17:38:10 +0000

    ponle color a la cara

--- src/components/mocking-duck.tsx
+++ src/components/mocking-duck.tsx
@@ -60,12 +60,17 @@ export function MockingDuck({ className }: { className?: string }) {
             className="w-full h-full"
             preserveAspectRatio="xMidYMid meet"
           >
-            <g fill="none" stroke="black" strokeWidth="4">
+            <g stroke="black" strokeWidth="4">
               {/* Face Outline */}
-              <path d="M165,65 C185,80 185,120 160,150 C130,190 60,195 40,155 C20,115 20,70 50,40 C80,10 140,25 165,65 Z" strokeWidth="6" />
+              <path d="M165,65 C185,80 185,120 160,150 C130,190 60,195 40,155 C20,115 20,70 50,40 C80,10 140,25 165,65 Z" strokeWidth="6" fill="#F0F0F0" />
 
-              {/* Smile */}
-              <path d="M45,100 C60,145 130,145 150,100" strokeWidth="8" />
+              {/* Teeth Fill (Mouth Shadow first) */}
+              <path d="M50,102 C60,140 130,140 145,102 L142,128 L132,133 L118,135 L105,136 L92,136 L80,135 L68,133 L55,128 Z" fill="#333333" stroke="none"/>
+              {/* Teeth Fill (White teeth on top) */}
+              <path d="M50,102 C60,140 130,140 145,102 L147,102 L135,102 L120,102 L105,102 L90,102 L75,102 L60,102 L48,103 Z" fill="white" stroke="none" />
+              
+              {/* Smile Outline */}
+              <path d="M45,100 C60,145 130,145 150,100" strokeWidth="8" fill="none"/>
               
               {/* Teeth Lines */}
               <path d="M48,103 L55,128" />
@@ -77,37 +82,45 @@ export function MockingDuck({ className }: { className?: string }) {
               <path d="M135,102 L132,133" />
               <path d="M147,102 L142,128" />
 
-              {/* Teeth Fill */}
-              <path d="M50,102 C60,140 130,140 145,102 L142,128 L132,133 L118,135 L105,136 L92,136 L80,135 L68,133 L55,128 Z" fill="black" stroke="none"/>
-              <path d="M50,102 C60,140 130,140 145,102 L147,102 L135,102 L120,102 L105,102 L90,102 L75,102 L60,102 L48,103 Z" fill="white" stroke="none" />
-              
               {/* Left Eye */}
-              <path d="M50,50 C65,40 85,45 90,60" />
-              <path d="M90,60 C80,75 60,75 50,60" />
-              <path d="M50,60 C60,55 75,55 80,60" />
+              <g fill="none">
+                <path d="M50,50 C65,40 85,45 90,60" />
+                <path d="M90,60 C80,75 60,75 50,60" />
+                <path d="M50,60 C60,55 75,55 80,60" />
+              </g>
 
               {/* Right Eye */}
-              <path d="M110,60 C120,45 145,45 155,65" />
-              <path d="M155,65 C145,80 120,80 110,70" />
-              <path d="M110,70 C120,68 135,68 145,70" />
+              <g fill="none">
+                <path d="M110,60 C120,45 145,45 155,65" />
+                <path d="M155,65 C145,80 120,80 110,70" />
+                <path d="M110,70 C120,68 135,68 145,70" />
+              </g>
               
               {/* Nose */}
-              <path d="M100,80 C110,90 105,105 95,100" />
-              <path d="M100,80 C90,90 95,105 105,100" />
-              <path d="M98,101 L108,101" />
+              <g fill="none">
+                <path d="M100,80 C110,90 105,105 95,100" />
+                <path d="M100,80 C90,90 95,105 105,100" />
+                <path d="M98,101 L108,101" />
+              </g>
 
               {/* Cheek Lines */}
-              <path d="M40,110 C30,115 25,125 40,130" />
-              <path d="M155,105 C168,110 170,120 158,128" />
-              <path d="M155,90 C165,92 168,98 160,102" />
-
+              <g fill="none">
+                <path d="M40,110 C30,115 25,125 40,130" />
+                <path d="M155,105 C168,110 170,120 158,128" />
+                <path d="M155,90 C165,92 168,98 160,102" />
+              </g>
+              
               {/* Chin Lines */}
-              <path d="M70,155 C85,165 110,165 125,155" />
-              <path d="M80,148 L115,148" />
+              <g fill="none">
+                <path d="M70,155 C85,165 110,165 125,155" />
+                <path d="M80,148 L115,148" />
+              </g>
 
               {/* Forehead Wrinkles */}
-              <path d="M60,35 C80,30 100,32 110,40" />
-              <path d="M70,45 C85,42 100,45 105,50" />
+              <g fill="none">
+                <path d="M60,35 C80,30 100,32 110,40" />
+                <path d="M70,45 C85,42 100,45 105,50" />
+              </g>
             </g>
           </svg>
         </div>
